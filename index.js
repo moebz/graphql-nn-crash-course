e8dba1ab9e598942e34073601620e1b50cec23ba
Moises B <mbaez091@gmail.com>
2024-05-15 18:56:01 -0400

    mutations

--- index.js
+++ index.js
@@ -10,12 +10,62 @@ const resolvers = {
     reviews() {
       return db.reviews;
     },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
     games() {
-      console.log("db", db);
       return db.games;
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
     },
     authors() {
       return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((author) => author.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((game) => game.id === parent.game_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter((game) => game.id !== args.id);
+      return db.games;
+    },
+    addGame(_, args) {
+      const newGame = {
+        ...args.game,
+        id: db.games.length + 1,
+      };
+      db.games.push(newGame);
+      return newGame;
+    },
+    updateGame(_, args) {
+      db.games = db.games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits };
+        }
+        return g;
+      });
+
+      return db.games.find((g) => g.id === args.id);
     },
   },
 };
